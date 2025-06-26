37e9a51e70b7ab5d0bc7170be2ef4dc51a36481c
Michal Miotk <michal.miotk@intel.com>
2025-06-26 10:07:20 +0000

    wip

--- src/plugins/intel_gpu/src/graph/impls/cm/xetla_lstm_seq.cpp
+++ src/plugins/intel_gpu/src/graph/impls/cm/xetla_lstm_seq.cpp
@@ -160,8 +160,8 @@ protected:
 class LSTMImpl : public PrimitiveImplCM {
 public:
     DECLARE_OBJECT_TYPE_SERIALIZATION(ov::intel_gpu::cm::LSTMImpl)
-    Stage::Ptr lstm_loop = make_stage<XetlaLSTMLoopGenerator>();
     Stage::Ptr lstm_gemm = make_stage<XetlaLSTMGemmGenerator>();
+    Stage::Ptr lstm_loop = make_stage<XetlaLSTMLoopGenerator>();
 
     LSTMImpl() : PrimitiveImplOCL(LSTMSeqImplementationManager::get_type_info_static()) {}
     LSTMImpl(const program_node& node, const RuntimeParams& params) : LSTMImpl() {
